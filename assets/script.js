aae9c2df9f61c8b22a171c4961d0bb1c1d205094
Alex Lee <98978477+ayesslee@users.noreply.github.com>
2022-09-28 20:46:03 -0700

    added seatgeek info

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
-// last fm api key
-var lastKey = "0e57f4bfc2a20794c0480350ee316c12";
+//seatgeek client id
+var clientId = 'Mjk0MDgzNjF8MTY2NDQxODE0Mi4zNTAzNjUy';
+var artistName;
+
+fetch(`https://api.seatgeek.com/2/events?performers.slug=${artistName}&client_id=${clientId}`)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    // response.json(json);
+    console.log(json);
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+  
